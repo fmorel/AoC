fc10a81cbd4d7dcfd3b50a0819404b15eaa58ed6
FX Morel <fx.morel@amarisoft.com>
2018-12-30 16:09:51 +0100

    Day 5

--- day_5.c
+++ day_5.c
@@ -10,7 +10,7 @@ void __attribute__((noreturn)) error(char *s) {
 
 const char polarity_offset = 'A' - 'a';
 
-int adjacent_polarity(char *s, int pos)
+int adjacent_polarity(const char *s, int pos)
 {
     char d = s[pos] - s[pos+1];
     if (d == polarity_offset || d == -polarity_offset)
@@ -18,14 +18,64 @@ int adjacent_polarity(char *s, int pos)
     return 0;
 }
 
+
+size_t reduce_polymer_without_unit(const char *s, size_t len, char u)
+{
+    char *s1 = malloc(len);
+    char *s2 = malloc(len);
+    char *swap;
+    size_t len2 = 0, i, iter = 0;
+
+    //Remove unit and copy to s1
+    for (i = 0; i < len; i++) {
+        if (s[i] == u || s[i] == u + polarity_offset)
+            continue;
+        s1[len2++] = s[i];
+    }
+
+    printf("Remove unit %c len %ld -> %ld\n", u, len, len2);
+    len = len2;
+    len2 = 0;
+
+    //Reduce polymer
+    while (1) {
+        for (i = 0; i < len-1; i++) {
+            if (adjacent_polarity(s1, i)) {
+                i++;
+                continue;
+            }
+            s2[len2++] = s1[i];
+        }
+        if (i == len-1)
+            s2[len2++] = s1[len-1];
+        s2[len2] = '\0';
+
+        swap = s1;
+        s1 = s2;
+        s2 = swap;
+        if (len == len2)
+            break;
+        len = len2;
+        len2 = 0;
+        iter++;
+    }
+
+    printf("%ld iterations, final length is %ld\n", iter, len);
+    free(s2);
+    free(s1);
+    return len;
+}
+
+
+
 int main(void)
 {
 
     /* Parse */
     FILE *f = fopen("day_5.input", "r");
-    char *s = NULL, *s2, *swap;
-    size_t len = 0, len2 = 0, i;
-    int iter = 0;
+    char *s = NULL;
+    size_t len = 0, cur_len, min_len;
+    char c, min_c;
     ssize_t ret;
 
     ret = getline(&s, &len, f);
@@ -37,34 +87,19 @@ int main(void)
     //Replace new_line with null char
     len = ret-1;
     s[len] ='\0';
-    s2 = malloc(len);
+    
+    printf("Part 1 answer is %ld\n", reduce_polymer_without_unit(s, len, '0'));
 
-    while (1) {
-        for (i = 0; i < len-1; i++) {
-            if (adjacent_polarity(s, i)) {
-                i++;
-                continue;
-            }
-            s2[len2++] = s[i];
+    min_len = len;
+    for (c = 'a'; c <= 'z'; c++) {
+        cur_len = reduce_polymer_without_unit(s, len, c);
+        if (cur_len < min_len) {
+            min_len = cur_len;
+            min_c = c;
         }
-        if (i == len-1)
-            s2[len2++] = s[len-1];
-        s2[len2] = '\0';
-
-        swap = s;
-        s = s2;
-        s2 = swap;
-        if (len == len2)
-            break;
-        len = len2;
-        len2 = 0;
-        iter++;
-        printf("Pass %d, new length is %ld\n", iter, len);
     }
-
-    printf("Result is length %ld\nvalue = %s\n", strlen(s), s);
+    printf("Minimum length is %ld (removing unit %c)", min_len, min_c);
     free(s);
-    free(s2);
 
     return 0;
 }
