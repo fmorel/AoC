44ac4dfd6f7b758a70596deb6d37f5a8aec432a8
FX Morel <fx.morel@amarisoft.com>
2022-12-05 10:12:58 +0100

    Add day2

--- 2022/main.rs
+++ 2022/main.rs
@@ -1,7 +1,11 @@
+#![allow(dead_code)]
+
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 use std::path::Path;
 
+
+/* Day 1 */
 #[derive(Copy, Clone)]
 struct Elf
 {
@@ -43,7 +47,53 @@ fn day_1(filename: &Path)
     println!("The 3 best elvs carry a total of {} calories", calories_3elves);
 }
 
+/* Day 2 */
+fn rps_index(play :&str) -> i32
+{
+    match play {
+        "A X" => 0,
+        "A Y" => 1,
+        "A Z" => 2,
+        "B X" => 3,
+        "B Y" => 4,
+        "B Z" => 5,
+        "C X" => 6,
+        "C Y" => 7,
+        "C Z" => 8,
+        ""    => 9,
+        _     => -1
+    }
+}
+
+fn rps_score(play :&str, day2: bool) -> u32
+{
+    /* Add the 'blank line' at index 9 */
+    let score_tab = [1+3, 2+6, 3+0, 1+0, 2+3, 3+6, 1+6, 2+0, 3+3, 0];
+    let score_tab_2 = [3+0, 1+3, 2+6, 1+0, 2+3, 3+6, 2+0, 3+3, 1+6, 0];
+    let index = rps_index(play);
+    assert!(index >= 0);
+    if day2 {
+        score_tab_2[index as usize]
+    } else {
+        score_tab[index as usize]
+    }
+}
+
+fn day_2(filename: &Path)
+{
+    let file = File::open(filename)
+                .expect("Could not open file");
+    let lines = BufReader::new(file).lines();
+    let mut points = 0;
+    for line in lines {
+        points += rps_score(line.unwrap().as_str(), true);
+    }
+    println!("Total points is {}", points);
+}
+
+/* Main */
 fn main()
 {
-    day_1(Path::new("inputs/day1.txt"));
+    //day_1(Path::new("inputs/day1.txt"));
+    day_2(Path::new("inputs/day2.txt"));
 }
