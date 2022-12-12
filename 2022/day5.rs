a67ccc3c5069fcbb834b089b21903275b6a9911f
FX Morel <efix.morel@gmail.com>
2022-12-12 23:39:46 +0100

    Add day5 (part 1)

---
+++ 2022/day5.rs
@@ -0,0 +1,67 @@
+
+
+use file_utils::file_to_lines;
+use std::path::Path;
+
+fn parse_initial_stacks(stacks: &mut [Vec<u8>], line: &str) -> bool
+{
+    let chars = line.as_bytes();
+    let mut i = 1;
+    let mut header = true;
+    for s in stacks.iter_mut() {
+        match chars[i] {
+            b'A'..=b'Z' => s.insert(0, chars[i]),
+            b' '       => (),
+            b'1'..=b'9' => header = false,
+            _         => println!("Unexpected character {} on header", chars[i])
+        }
+        i += 4;
+    }
+    if !header {
+        print!("Initial stack top: ");
+        print_stack_top(stacks);
+    }
+    header
+}
+
+fn parse_move(stacks: &mut [Vec<u8>], line: &str)
+{
+    let tokens = line.split_ascii_whitespace();
+    let moves: Vec<usize> = tokens.filter_map(|t| t.parse::<usize>().ok()).collect();
+    if moves.len() < 3 {
+        return;
+    }
+    //moves : number, stack start, stack end (and index them from 0)
+    let (n, s, e) = (moves[0], moves[1] - 1 , moves[2] - 1);
+    for _i in 0..n {
+        let item = stacks[s].pop().expect("Unstacking empty stack !");
+        stacks[e].push(item);
+    }
+    /* Debug */
+    //print!("After move {} f {} to {}: ", n, s, e);
+    //print_stack_top(stacks);
+}
+
+fn print_stack_top(stacks: &[Vec<u8>])
+{
+    for s in stacks.iter() {
+        print!("{}", *s.last().unwrap_or(&b'*') as char);
+    }
+    println!("");
+}
+
+pub fn day5(filename: &Path)
+{
+    let mut stacks: [Vec<u8>; 9] = Default::default();
+    let mut header = true;
+    let lines = file_to_lines(filename);
+    for line in lines {
+        if header {
+            header = parse_initial_stacks(&mut stacks, line.as_str());
+        } else {
+            parse_move(&mut stacks, line.as_str());
+        }
+    }
+    print!("Final stack top: ");
+    print_stack_top(&stacks);
+}
--- 2022/main.rs
+++ 2022/main.rs
@@ -4,7 +4,9 @@ use std::path::Path;
 /* Create modules */
 mod file_utils;
 mod day1_to_4;
+mod day5;
 use day1_to_4::{day_1, day_2, day_3, day_4};
+use day5::day5;
 
 /* Main */
 fn main()
@@ -12,5 +14,6 @@ fn main()
     //day_1(Path::new("inputs/day1.txt"));
     //day_2(Path::new("inputs/day2.txt"));
     //day_3(Path::new("inputs/day3.txt"), true);
-    day_4(Path::new("inputs/day4.txt"), true);
+    //day_4(Path::new("inputs/day4.txt"), true);
+    day5(Path::new("inputs/day5.txt"));
 }
